39765202485eab6b9016eceb68b3639dfa30a1c3
KKocot <k.kocot0@gmail.com>
2024-05-19 19:41:05 +0200

    fix: resolve image loading issue

--- src/components/radio-component.tsx
+++ src/components/radio-component.tsx
@@ -13,6 +13,49 @@ type FormColData = {
   era: string;
 };
 
+const Componeny = ({
+  unitValue,
+  era,
+  label,
+  value,
+}: {
+  unitValue: string;
+  era: string;
+  label: string;
+  value: string;
+}) => {
+  return (
+    <FormLabel
+      className={clsx(
+        "p-1 w-full bg-slate-600 font-semibold rounded-sm text-white cursor-pointer hover:bg-slate-600 transition duration-300 ease-in-out transform",
+        {
+          "bg-gradient-to-r from-yellow-300 to-yellow-800 text-slate-800":
+            unitValue === value && era === "golden",
+          "hover:bg-gradient-to-r hover:to-yellow-300 hover:from-yellow-800 hover:text-white font-semibold":
+            era === "golden",
+          "bg-gradient-to-r from-violet-300 to-violet-800 text-slate-800":
+            unitValue === value && era === "heroic",
+          "hover:bg-gradient-to-r hover:to-violet-300 hover:from-violet-800 hover:text-white font-semibold":
+            era === "heroic",
+          "bg-gradient-to-r from-green-300 to-green-800 text-slate-800":
+            unitValue === value && era === "green",
+          "hover:bg-gradient-to-r hover:to-green-300 hover:from-green-800 hover:text-white font-semibold":
+            era === "green",
+          "bg-gradient-to-r from-blue-300 to-blue-800 text-slate-800":
+            unitValue === value && era === "blue",
+          "hover:bg-gradient-to-r hover:to-blue-300 hover:from-blue-800 hover:text-white font-semibold":
+            era === "blue",
+          "bg-gradient-to-r from-gray-300 to-gray-800 text-slate-800":
+            unitValue === value && era === "grey",
+          "hover:bg-gradient-to-r hover:to-gray-300 hover:from-gray-800 hover:text-white font-semibold":
+            era === "grey",
+        }
+      )}
+    >
+      {label}
+    </FormLabel>
+  );
+};
 const RadioComponent: FC<{
   unitData: FormColData;
   controller: any;
@@ -39,137 +82,46 @@ const RadioComponent: FC<{
               <FormControl>
                 <RadioGroupItem value="0" className="hidden" />
               </FormControl>
-              <FormLabel
-                className={clsx(
-                  "p-1 w-full bg-slate-600 font-semibold rounded-sm text-white cursor-pointer hover:bg-slate-600 transition duration-300 ease-in-out transform",
-                  {
-                    "bg-gradient-to-r from-yellow-300 to-yellow-800 text-slate-800":
-                      field.value === "0" && unitData.era === "golden",
-                    "hover:bg-gradient-to-r hover:to-yellow-300 hover:from-yellow-800 hover:text-white font-semibold":
-                      unitData.era === "golden",
-                    "bg-gradient-to-r from-violet-300 to-violet-800 text-slate-800":
-                      field.value === "0" && unitData.era === "heroic",
-                    "hover:bg-gradient-to-r hover:to-violet-300 hover:from-violet-800 hover:text-white font-semibold":
-                      unitData.era === "heroic",
-                    "bg-gradient-to-r from-green-300 to-green-800 text-slate-800":
-                      field.value === "0" && unitData.era === "green",
-                    "hover:bg-gradient-to-r hover:to-green-300 hover:from-green-800 hover:text-white font-semibold":
-                      unitData.era === "green",
-                    "bg-gradient-to-r from-blue-300 to-blue-800 text-slate-800":
-                      field.value === "0" && unitData.era === "blue",
-                    "hover:bg-gradient-to-r hover:to-blue-300 hover:from-blue-800 hover:text-white font-semibold":
-                      unitData.era === "blue",
-                    "bg-gradient-to-r from-gray-300 to-gray-800 text-slate-800":
-                      field.value === "0" && unitData.era === "grey",
-                    "hover:bg-gradient-to-r hover:to-gray-300 hover:from-gray-800 hover:text-white font-semibold":
-                      unitData.era === "grey",
-                  }
-                )}
-              >
-                Nie mam
-              </FormLabel>
+
+              <Componeny
+                unitValue={field.value}
+                era={unitData.era}
+                label="Nie mam"
+                value="0"
+              />
             </FormItem>
             <FormItem className="flex items-center space-y-0">
               <FormControl>
                 <RadioGroupItem value="1" className="hidden" />
               </FormControl>
-              <FormLabel
-                className={clsx(
-                  "p-1 w-full bg-slate-600 font-semibold rounded-sm text-white cursor-pointer hover:bg-slate-600 transition duration-300 ease-in-out transform",
-                  {
-                    "bg-gradient-to-r from-yellow-300 to-yellow-800 text-slate-800":
-                      field.value === "1" && unitData.era === "golden",
-                    "hover:bg-gradient-to-r hover:to-yellow-300 hover:from-yellow-800 hover:text-white font-semibold":
-                      unitData.era === "golden",
-                    "bg-gradient-to-r from-violet-300 to-violet-800 text-slate-800":
-                      field.value === "1" && unitData.era === "heroic",
-                    "hover:bg-gradient-to-r hover:to-violet-300 hover:from-violet-800 hover:text-white font-semibold":
-                      unitData.era === "heroic",
-                    "bg-gradient-to-r from-green-300 to-green-800 text-slate-800":
-                      field.value === "1" && unitData.era === "green",
-                    "hover:bg-gradient-to-r hover:to-green-300 hover:from-green-800 hover:text-white font-semibold":
-                      unitData.era === "green",
-                    "bg-gradient-to-r from-blue-300 to-blue-800 text-slate-800":
-                      field.value === "1" && unitData.era === "blue",
-                    "hover:bg-gradient-to-r hover:to-blue-300 hover:from-blue-800 hover:text-white font-semibold":
-                      unitData.era === "blue",
-                    "bg-gradient-to-r from-gray-300 to-gray-800 text-slate-800":
-                      field.value === "1" && unitData.era === "grey",
-                    "hover:bg-gradient-to-r hover:to-gray-300 hover:from-gray-800 hover:text-white font-semibold":
-                      unitData.era === "grey",
-                  }
-                )}
-              >
-                Mam
-              </FormLabel>
+              <Componeny
+                unitValue={field.value}
+                era={unitData.era}
+                label="Mam"
+                value="1"
+              />
             </FormItem>
             <FormItem className="flex items-center space-y-0">
               <FormControl>
                 <RadioGroupItem value="2" className="hidden" />
               </FormControl>
-              <FormLabel
-                className={clsx(
-                  "p-1 w-full bg-slate-600 font-semibold rounded-sm text-white cursor-pointer hover:bg-slate-600 transition duration-300 ease-in-out transform",
-                  {
-                    "bg-gradient-to-r from-yellow-300 to-yellow-800 text-slate-800":
-                      field.value === "2" && unitData.era === "golden",
-                    "hover:bg-gradient-to-r hover:to-yellow-300 hover:from-yellow-800 hover:text-white font-semibold":
-                      unitData.era === "golden",
-                    "bg-gradient-to-r from-violet-300 to-violet-800 text-slate-800":
-                      field.value === "2" && unitData.era === "heroic",
-                    "hover:bg-gradient-to-r hover:to-violet-300 hover:from-violet-800 hover:text-white font-semibold":
-                      unitData.era === "heroic",
-                    "bg-gradient-to-r from-green-300 to-green-800 text-slate-800":
-                      field.value === "2" && unitData.era === "green",
-                    "hover:bg-gradient-to-r hover:to-green-300 hover:from-green-800 hover:text-white font-semibold":
-                      unitData.era === "green",
-                    "bg-gradient-to-r from-blue-300 to-blue-800 text-slate-800":
-                      field.value === "2" && unitData.era === "blue",
-                    "hover:bg-gradient-to-r hover:to-blue-300 hover:from-blue-800 hover:text-white font-semibold":
-                      unitData.era === "blue",
-                    "bg-gradient-to-r from-gray-300 to-gray-800 text-slate-800":
-                      field.value === "2" && unitData.era === "grey",
-                    "hover:bg-gradient-to-r hover:to-gray-300 hover:from-gray-800 hover:text-white font-semibold":
-                      unitData.era === "grey",
-                  }
-                )}
-              >
-                Wymaksowana
-              </FormLabel>
+              <Componeny
+                unitValue={field.value}
+                era={unitData.era}
+                label="Wymaksowana"
+                value="2"
+              />
             </FormItem>
             <FormItem className="flex items-center space-y-0">
               <FormControl>
                 <RadioGroupItem value="3" className="hidden" />
               </FormControl>
-              <FormLabel
-                className={clsx(
-                  "p-1 w-full bg-slate-600 font-semibold rounded-sm text-white cursor-pointer hover:bg-slate-600 transition duration-300 ease-in-out transform",
-                  {
-                    "bg-gradient-to-r from-yellow-300 to-yellow-800 text-slate-800":
-                      field.value === "3" && unitData.era === "golden",
-                    "hover:bg-gradient-to-r hover:to-yellow-300 hover:from-yellow-800 hover:text-white font-semibold":
-                      unitData.era === "golden",
-                    "bg-gradient-to-r from-violet-300 to-violet-800 text-slate-800":
-                      field.value === "3" && unitData.era === "heroic",
-                    "hover:bg-gradient-to-r hover:to-violet-300 hover:from-violet-800 hover:text-white font-semibold":
-                      unitData.era === "heroic",
-                    "bg-gradient-to-r from-green-300 to-green-800 text-slate-800":
-                      field.value === "3" && unitData.era === "green",
-                    "hover:bg-gradient-to-r hover:to-green-300 hover:from-green-800 hover:text-white font-semibold":
-                      unitData.era === "green",
-                    "bg-gradient-to-r from-blue-300 to-blue-800 text-slate-800":
-                      field.value === "3" && unitData.era === "blue",
-                    "hover:bg-gradient-to-r hover:to-blue-300 hover:from-blue-800 hover:text-white font-semibold":
-                      unitData.era === "blue",
-                    "bg-gradient-to-r from-gray-300 to-gray-800 text-slate-800":
-                      field.value === "3" && unitData.era === "grey",
-                    "hover:bg-gradient-to-r hover:to-gray-300 hover:from-gray-800 hover:text-white font-semibold":
-                      unitData.era === "grey",
-                  }
-                )}
-              >
-                Preferuje
-              </FormLabel>
+              <Componeny
+                unitValue={field.value}
+                era={unitData.era}
+                label="Preferuje"
+                value="3"
+              />
             </FormItem>
           </RadioGroup>
         )}
