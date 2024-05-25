b35ebc8228fb16e27c26e1e01c42d141843d9687
KKocot <k.kocot0@gmail.com>
2024-05-25 14:57:03 +0200

    chore: Update form page and add whitelist functionality

--- src/app/(protected)/form/page.tsx
+++ src/app/(protected)/form/page.tsx
@@ -2,8 +2,29 @@
 
 import UnitsForm from "@/components/units-form";
 import React, { useEffect, useState } from "react";
+import { useSession } from "next-auth/react";
 
 const Page: React.FC = () => {
+  const [whitelist, setWhitelist] = useState([]);
+  const { data } = useSession();
+
+  useEffect(() => {
+    const fetchWhitelist = async () => {
+      try {
+        const response = await fetch("/api/whitelist");
+        const data = await response.json();
+        setWhitelist(data.whitelist);
+      } catch (error) {
+        console.error("Error fetching whitelist:", error);
+      }
+    };
+
+    fetchWhitelist();
+  }, []);
+  if (whitelist.length === 0 && !data) {
+    <div>loading</div>;
+  }
   return <UnitsForm />;
 };
+
 export default Page;
