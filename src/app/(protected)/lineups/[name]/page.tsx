d10054996e78154c5215a92c9635f3cf055685b0
KKocot <k.kocot0@gmail.com>
2024-06-01 19:31:08 +0200

    Refactor UI components to include Checkbox and Accordion functionality

--- src/app/(protected)/lineups/[name]/page.tsx
+++ src/app/(protected)/lineups/[name]/page.tsx
@@ -1,35 +1,90 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
+import { goldenUnits } from "@/assets/golden-units-data";
+import { Autocompleter } from "@/components/autocompleter";
+import { Input } from "@/components/ui/input";
 import {
-  Table,
-  TableBody,
-  TableCaption,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import { Checkbox } from "@/components/ui/checkbox";
+const form = [
+  "User 1",
+  "User 2",
+  "User 3",
+  "User 4",
+  "User 5",
+  "User 6",
+  "User 7",
+  "User 8",
+  "User 9",
+  "User 10",
+  "User 11",
+  "User 12",
+  "User 13",
+  "User 14",
+  "User 15",
+];
+const Item = ({ index, name }: { index: number; name: string }) => {
+  const [value, setValue] = useState("");
+  return (
+    <li className="grid grid-cols-11 border-2 border-primary p-2 rounded-2xl items-center">
+      <span className="col-span-1">{index + 1}</span>
+      <span className="col-span-2">
+        <Input value={name} className="p-1" />
+      </span>
+      <span className="col-span-2">
+        <Autocompleter value={value} onChange={setValue} items={goldenUnits} />
+      </span>
+      <span className="col-span-2">
+        <Autocompleter value={value} onChange={setValue} items={goldenUnits} />
+      </span>
+      <span className="col-span-2">
+        <Autocompleter value={value} onChange={setValue} items={goldenUnits} />
+      </span>
+      <span className="col-span-2 text-center">Opis</span>
+    </li>
+  );
+};
+
+const CheckboxItem = ({ label }: { label: string }) => {
+  return (
+    <AccordionContent>
+      <div className="flex items-center space-x-2">
+        <Checkbox id={label} />
+        <label
+          htmlFor={label}
+          className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+        >
+          {label}
+        </label>
+      </div>
+    </AccordionContent>
+  );
+};
 
 const Page: React.FC = () => {
   return (
-    <Table>
-      <TableCaption>A list of your recent invoices.</TableCaption>
-      <TableHeader>
-        <TableRow>
-          <TableHead className="w-[100px]">Invoice</TableHead>
-          <TableHead>Status</TableHead>
-          <TableHead>Method</TableHead>
-          <TableHead className="text-right">Amount</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        <TableRow>
-          <TableCell className="font-medium">INV001</TableCell>
-          <TableCell>Paid</TableCell>
-          <TableCell>Credit Card</TableCell>
-          <TableCell className="text-right">$250.00</TableCell>
-        </TableRow>
-      </TableBody>
-    </Table>
+    <div className="flex flex-col gap-5 p-2">
+      <Accordion type="single" collapsible>
+        <AccordionItem value="item-1">
+          <AccordionTrigger>Filtry</AccordionTrigger>
+          <AccordionContent className="flex justify-between p-2">
+            <CheckboxItem label="Zlota era" />
+            <CheckboxItem label="Heroic era" />
+            <CheckboxItem label="Reszta" />
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
+      <ul className="flex flex-col gap-2">
+        {form.map((e, index) => (
+          <Item index={index} name={e} />
+        ))}
+      </ul>
+    </div>
   );
 };
 
---
+++ src/components/autocompleter.tsx
@@ -0,0 +1,96 @@
+import {
+  Command,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command";
+import { useEffect, useRef, useState } from "react";
+import { AvatarImage, AvatarFallback, Avatar } from "@/components/ui/avatar";
+import { Checkbox } from "@/components/ui/checkbox";
+
+interface Unit {
+  era: string;
+  icon: string;
+  id: number;
+  leadership: number;
+  masteryPoints: boolean;
+  name: string;
+  src: string;
+  value: number;
+}
+
+export function Autocompleter({
+  value,
+  onChange,
+  items,
+}: {
+  value: string;
+  onChange: (e: string) => void;
+  items: Unit[];
+}) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const inputRef = useRef<HTMLInputElement | null>(null);
+  const autocompleteRef = useRef<HTMLDivElement | null>(null);
+  const handleInputFocus = () => {
+    setIsOpen(true);
+  };
+  const handleClickOutside = (event: MouseEvent) => {
+    if (
+      autocompleteRef.current &&
+      !autocompleteRef.current.contains(event.target as Node)
+    ) {
+      setIsOpen(false);
+    }
+  };
+  useEffect(() => {
+    document.addEventListener("click", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, []);
+
+  return (
+    <Command className="w-full border" ref={autocompleteRef}>
+      <CommandInput
+        ref={inputRef}
+        onFocus={handleInputFocus}
+        value={value}
+        onValueChange={(e) => onChange(e)}
+      />
+      <CommandList>
+        {isOpen && (
+          <CommandGroup className="absolute max-h-36 overflow-scroll bg-primary">
+            {items &&
+              items.map((item) => (
+                <CommandItem
+                  key={item.id}
+                  className="p-0"
+                  onSelect={() => onChange(item.name)}
+                >
+                  <div
+                    onClick={() => onChange(item.name)}
+                    className="w-56 px-2 py-1 even:bg-black flex items-center gap-2"
+                    title={item.name}
+                  >
+                    <Avatar
+                      className="h-8 w-8 md:h-12 md:w-12"
+                      title={item.name}
+                    >
+                      <AvatarImage alt={item.name} src={item.icon} />
+                      <AvatarFallback>
+                        <img src={item.src} />
+                      </AvatarFallback>
+                    </Avatar>
+                    <span>{item.name}</span>
+                  </div>
+                </CommandItem>
+              ))}
+          </CommandGroup>
+        )}
+      </CommandList>
+    </Command>
+  );
+}
