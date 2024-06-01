38ea9af0ba5bac80cd61e59f4497460acdb51f09
KKocot <k.kocot0@gmail.com>
2024-06-01 23:17:11 +0200

    chore: Refactor signup schema and model to use more descriptive field names

--- src/app/api/signup/shema.ts
+++ src/app/api/signup/shema.ts
@@ -2,8 +2,8 @@ import z from "zod";
 
 export const putSignUpSchema = z.object({
   date: z.string(),
-  1: z.array(z.string()),
-  2: z.array(z.string()),
-  3: z.array(z.string()),
-  4: z.array(z.string()),
+  lineup_1: z.array(z.string()),
+  lineup_2: z.array(z.string()),
+  lineup_3: z.array(z.string()),
+  lineup_4: z.array(z.string()),
 });
--- src/models/signup.ts
+++ src/models/signup.ts
@@ -2,10 +2,10 @@ import mongoose, { Schema } from "mongoose";
 
 const SignUpSchema = new Schema({
   date: String,
-  1: [String],
-  2: [String],
-  3: [String],
-  4: [String],
+  lineup_1: [String],
+  lineup_2: [String],
+  lineup_3: [String],
+  lineup_4: [String],
 });
 
 const SignUp = mongoose.models.SignUp || mongoose.model("SignUp", SignUpSchema);
