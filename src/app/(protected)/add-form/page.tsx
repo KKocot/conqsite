9e93e20a5dbf7209ea316317e4684245b1a9a2dc
KKocot <k.kocot0@gmail.com>
2024-06-22 16:57:46 +0200

    feat: Implement command and user whitelists
    The code changes include adding a new file `whitelists.ts` that contains two arrays: `command_whitelist` and `adduser_whitelist`. The `command_whitelist` array is imported in the `page.tsx` file and used in the code. This change is necessary to implement whitelisting functionality for commands and users.

--- src/app/(protected)/add-form/page.tsx
+++ src/app/(protected)/add-form/page.tsx
@@ -2,11 +2,15 @@
 
 import { Input } from "@/components/ui/input";
 import UnitsForm from "@/components/units-form";
+import { useSession } from "next-auth/react";
 import React from "react";
-
+import { adduser_whitelist } from "@/assets/whitelists";
 const Page: React.FC = () => {
+  const { data } = useSession();
+  const user_id = data ? data?.user.id : "";
   const [discordId, setDiscordId] = React.useState<string>("");
-  return (
+  console.log(adduser_whitelist.includes(user_id));
+  return adduser_whitelist.includes(user_id) ? (
     <div>
       <Input
         placeholder="Discord ID"
@@ -16,6 +20,8 @@ const Page: React.FC = () => {
       />
       {discordId.length > 17 ? <UnitsForm user_id={discordId} /> : null}
     </div>
+  ) : (
+    <div className="flex justify-center my-56">Nie masz dostepu</div>
   );
 };
 export default Page;
