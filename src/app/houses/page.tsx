45db2074ec29869e22578a042728e3f2c400a559
KKocot <k.kocot0@gmail.com>
2024-09-14 11:15:12 +0200

    Refactor house settings form, add disabled state to input fields, and update house details component

--- src/app/houses/page.tsx
+++ src/app/houses/page.tsx
@@ -1,7 +1,9 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { useEffect, useState } from "react";
+import { toast } from "react-toastify";
 
 export interface HouseProps {
   name: string;
@@ -28,17 +30,42 @@ const HousesPage = () => {
   return (
     <div className="flex flex-col items-center">
       <h1 className="m-10">Houses</h1>
-      <ul className="flex justify-around w-full">
+      <ul className="flex gap-12 flex-wrap m-12 justify-center">
         {houses.map((house) => (
-          <li key={house.name}>
-            <h2>{house.name}</h2>
-            <img src={house.avatar} alt="House Avatar" />
-            <Link target="_blank" href={house.discordLink}>
-              Join Discord
-            </Link>
-            <p>{house.description}</p>
+          <li
+            className="border border-sky-500 flex flex-col justify-between items-center gap-2 w-[180px] rounded-sm"
+            key={house.name}
+          >
+            <div>
+              <h2 className="text-xl text-center p-2">{house.name}</h2>
+              <img
+                className="w-[180px] h-[180px]"
+                src={house.avatar}
+                alt="House Avatar"
+              />
+            </div>
+            <p className="px-2 text-sm">{house.description}</p>
             <p>{house.country}</p>
             <p>{house.server}</p>
+            <div>
+              <Link className="w-full" target="_blank" href={house.discordLink}>
+                <Button
+                  variant="tab"
+                  className="w-full text-black dark:text-white hover:text-white"
+                >
+                  Join Discord
+                </Button>
+              </Link>
+              <Button
+                variant="tab"
+                className="w-full text-black dark:text-white hover:text-white"
+                onClick={() => {
+                  toast.error("Not working yet");
+                }}
+              >
+                Join to House
+              </Button>
+            </div>
           </li>
         ))}
       </ul>
