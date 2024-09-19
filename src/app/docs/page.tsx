89790504f9c147a1157f070beca34c1f9bcb9b65
KKocot <k.kocot0@gmail.com>
2024-09-19 15:28:59 +0200

    Refactor language translations for Konquerus Bot User Guide

--- src/app/docs/page.tsx
+++ src/app/docs/page.tsx
@@ -7,8 +7,8 @@ const DocsPage: React.FC = () => {
   return (
     <div className="container mx-auto p-6">
       <h1 className="text-3xl font-bold mb-6">{t("documentation.title")}</h1>
-      <div className="container mx-auto p-6">
-        <div className="p-6 rounded-lg shadow-lg">
+      <div className="container mx-auto p-6 shadow-lg">
+        <div className="p-6 rounded-lg">
           <h2 className="text-2xl font-semibold mb-4">{t("guide.title")}</h2>
           <ol className="list-decimal list-inside space-y-4">
             <li>
@@ -117,6 +117,88 @@ const DocsPage: React.FC = () => {
             </li>
           </ol>
         </div>
+        <h1 className="text-2xl font-bold ">{t("guide_2.title")}</h1>
+        <img
+          src="docs/image1.png"
+          alt="Example Image 1"
+          className="max-w-full h-auto"
+        />
+
+        <p>
+          <strong>{t("guide_2.slash_command")}</strong>
+        </p>
+        <h2 className="text-xl font-semibold  mt-4">{t("guide_2.menu_eng")}</h2>
+        <p>{t("guide_2.menu_eng_description")}</p>
+        <ul className="list-disc ml-5">
+          <li>
+            <strong>{t("guide_2.send_presence")}</strong>
+          </li>
+          <li>
+            <strong>{t("guide_2.quick_add")}</strong>
+          </li>
+          <li>
+            <strong>{t("guide_2.delete_player")}</strong>
+          </li>
+          <li>
+            <strong>{t("guide_2.add_all")}</strong>
+          </li>
+          <li>
+            <strong>{t("guide_2.verification")}</strong>
+          </li>
+        </ul>
+        <img
+          src="docs/image3.png"
+          alt="Example Image 1"
+          className="max-w-full h-auto"
+        />
+        <h2 className="text-xl font-semibold mt-4">{t("guide_2.functions")}</h2>
+        <p>{t("guide_2.functions_description")}</p>
+
+        <h2 className="text-xl font-semibold mt-4">
+          {t("guide_2.example_functions")}
+        </h2>
+        <ul className="list-disc ml-5">
+          <li>{t("guide_2.auto_roles")}</li>
+          <li>{t("guide_2.check_logs")}</li>
+          <li>{t("guide_2.create_presence_lists")}</li>
+        </ul>
+        <h2 className="text-xl font-semibold mt-4">{t("guide_2.warthog")}</h2>
+        <p>{t("guide_2.warthog_description")}</p>
+        <p>
+          <em>{t("guide_2.warthog_example")}</em>
+        </p>
+
+        <div className="image my-5 text-center">
+          <img
+            src="docs/image.png"
+            alt="Example Image 1"
+            className="max-w-full h-auto"
+          />
+        </div>
+
+        <h2 className="text-xl font-semibold mt-4">
+          {t("guide_2.installation_instructions")}
+        </h2>
+        <p>{t("guide_2.installation_steps")}</p>
+        <ol className="list-decimal ml-5">
+          <li>{t("guide_2.step1")}</li>
+          <li>{t("guide_2.step2")}</li>
+          <li>{t("guide_2.step3")}</li>
+          <li>{t("guide_2.step4")}</li>
+        </ol>
+
+        <h2 className="text-xl font-semibold mt-4">
+          {t("guide_2.polish_version_functions")}
+        </h2>
+        <ul className="list-disc ml-5">
+          <li>{t("guide_2.create_event_groups")}</li>
+          <li>{t("guide_2.auto_verification")}</li>
+        </ul>
+        <img
+          src="docs/image2.png"
+          alt="Example Image 1"
+          className="max-w-full h-auto"
+        />
       </div>
     </div>
   );
