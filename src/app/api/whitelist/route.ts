37b4763e0deac492259f6ee2b0789161c298d046
KKocot <k.kocot0@gmail.com>
2024-09-19 21:12:48 +0200

    Refactor DELETE function in survey route

--- src/app/api/whitelist/route.ts
+++ src/app/api/whitelist/route.ts
@@ -1,6 +1,6 @@
 import connectMongoDB from "@/lib/mongodb";
 import Whitelist from "@/models/whitelist";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { putWhitelistSchema } from "./shema";
 import { ZodError } from "zod";
 import { headers } from "next/headers";
@@ -53,3 +53,28 @@ export async function GET(request: Request) {
   }
 }
 //TODO: GET by only ID one user
+export async function DELETE(request: NextRequest) {
+  const discordKey = request.headers.get("discord-key");
+
+  // Allow access only to the Discord Bot
+  if (!discordKey || discordKey !== process.env.BOT_KEY)
+    return new Response("401");
+
+  const { searchParams } = new URL(request.url);
+  const id = searchParams.get("id");
+
+  if (!id) {
+    return NextResponse.json({ message: "ID is required" }, { status: 400 });
+  }
+
+  try {
+    await connectMongoDB();
+    await Whitelist.findOneAndDelete({ idDiscord: id });
+    return NextResponse.json({ message: "User deleted" }, { status: 200 });
+  } catch (error) {
+    if (error instanceof ZodError)
+      return NextResponse.json({ message: error.message }, { status: 400 });
+    if (error instanceof Error)
+      return NextResponse.json({ message: error.message }, { status: 500 });
+  }
+}
