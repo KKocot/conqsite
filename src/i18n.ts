54096344f4e86de02e6d15de79a15ccc42ffb7a6
Kacper Kruczek <kacper.m.kruczek@email.com>
2024-09-21 11:02:08 +0200

    falback to default language if key is missing

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,5 +1,6 @@
 import { getRequestConfig } from "next-intl/server";
 import { getUserLocale } from "./lib/locale";
+import { assign } from "radash";
 
 export const locales = ["gb", "pl", "fr", "de", "tr", "es"] as const;
 export type Locale = (typeof locales)[number];
@@ -7,9 +8,13 @@ export const defaultLocale: Locale = "gb";
 
 export default getRequestConfig(async () => {
   const locale = await getUserLocale();
+  const [messages, defaultMessages] = await Promise.all([
+    import(`../messages/${locale}.json`),
+    import(`../messages/${defaultLocale}.json`),
+  ]);
   return {
     locale,
-    messages: (await import(`../messages/${locale}.json`)).default,
+    messages: assign(defaultMessages.default, messages.default),
     timeZone: "Europe/Warsaw",
   };
 });
