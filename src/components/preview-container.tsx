00f52064d30a6de690cf49f252368c4af0da0801
KKocot <k.kocot0@gmail.com>
2024-11-08 05:54:34 +0100

    feat: refactor useDeleteSheet hook to accept parameters in mutation and integrate with PreviewContainer for sheet deletion

--- src/components/preview-container.tsx
+++ src/components/preview-container.tsx
@@ -3,6 +3,8 @@ import { Button } from "./ui/button";
 import Preview from "./preview";
 import { Unit } from "@/lib/type";
 import { PublicLineup } from "@/lib/get-data";
+import useDeleteSheet from "./hooks/use-delete-sheet";
+import Loading from "react-loading";
 
 interface PreviewContainerProps {
   canDelete: boolean;
@@ -23,32 +25,21 @@ const PreviewContainer: FC<PreviewContainerProps> = ({
   data,
   username,
 }) => {
-  const onDelete = async (name: string) => {
+  const deleteSheetMutation = useDeleteSheet();
+
+  const onDelete = (name: string) => {
     const confirmed = confirm("Are you sure you want to delete this sheet?");
     if (confirmed) {
-      try {
-        const response = await fetch(
-          `/api/publicLineup?house=${house}&date=${date}&name=${name}`,
-          {
-            method: "DELETE",
-          }
-        );
-        if (!response.ok) {
-          const errorData = await response.json();
-          console.error("Error occurred:", errorData);
-        } else {
-          console.log("Success:", await response.json());
-        }
-      } catch (error) {
-        console.error("Error deleting:", error);
-      }
+      deleteSheetMutation.mutate({ house, date, name });
     }
   };
 
   return (
     <div className="flex gap-4 p-12">
       {data.map((e) =>
-        e.name === lineup ? (
+        deleteSheetMutation.isPending ? (
+          <Loading color="#94a3b8" />
+        ) : e.name === lineup ? (
           <div key={e.name}>
             <div className="flex items-center gap-5">
               <h1 className="text-2xl font-bold">Lineup: {e.name}</h1>
