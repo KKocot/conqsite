05c8a1f5ae1c1ff602b367dd6921a854f100797f
KKocot <k.kocot0@gmail.com>
2024-11-17 10:30:56 +0100

    event endpoint to manage sighup bot

---
+++ src/app/api/event/shema.ts
@@ -0,0 +1,14 @@
+import { z } from "zod";
+
+const signUpSchema = z.object({
+  name: z.string(),
+  status: z.string(),
+  userId: z.string(),
+});
+
+export const putEventSchema = z.object({
+  eventId: z.string(),
+  house: z.string(),
+  date: z.string(),
+  signUps: z.array(signUpSchema),
+});
