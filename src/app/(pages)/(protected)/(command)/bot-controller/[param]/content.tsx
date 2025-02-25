2df9d30c001ab431406bfb8e9b518587b4b8f413
Krzysztof Kocot <k.kocot0@gmail.com>
2025-02-25 17:16:33 +0100

    Refactor code and left comments

--- src/app/(pages)/(protected)/(command)/bot-controller/[param]/content.tsx
+++ src/app/(pages)/(protected)/(command)/bot-controller/[param]/content.tsx
@@ -9,12 +9,6 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger,
-} from "@/components/ui/tooltip";
 import EventDialog from "@/feature/bot-controller/dialog";
 import {
   getDiscordData,
@@ -23,24 +17,21 @@ import {
   BotEvent,
 } from "@/lib/get-data";
 import { useQuery } from "@tanstack/react-query";
-import { PenIcon, Trash2 } from "lucide-react";
-
-const Content = ({
-  house,
-  userId,
-  config,
-  events,
-}: {
+import { Trash2 } from "lucide-react";
+interface Props {
   house: string;
   userId: string;
   config: HouseSettings;
   events: BotEvent[];
-}) => {
+}
+const Content = ({ house, userId, config, events }: Props) => {
+  // Get house assets
   const { data: assets } = useQuery({
     queryKey: ["houseAssets", house],
     queryFn: () => getHouseAssets(house),
     enabled: !!house,
   });
+  // Data for discord server: channels, roles, users
   const { data: discordData, isLoading: discordDataLoading } = useQuery({
     queryKey: ["discordData", house],
     queryFn: () =>
@@ -52,75 +43,73 @@ const Content = ({
     enabled: !!house,
   });
   const deleteBotEventMutation = useDeleteBotEventMutation();
+
+  // Delete event
   const onDelete = (event: BotEvent) => {
-    deleteBotEventMutation.mutate({ id: event._id ?? "", house });
+    // event._id is undefined when creating new event
+    if (event._id) deleteBotEventMutation.mutate({ id: event._id, house });
   };
+
+  // Check if house is premium
   const premium = assets?.premium ?? false;
-  const limited = (events.length ?? 0) >= (premium ? 6 : 7);
+
+  // Check if if limited events is reached
+  const limited = (events.length ?? 0) >= (premium ? 3 : 6);
+
+  // Check if events list is empty
+  if (events.length === 0) return <div>No events found</div>;
+
   return (
     <div className="container py-8 px-4">
       <div className="flex gap-4 flex-wrap">
-        {events.length === 0 ? (
-          <div>No events found</div>
-        ) : (
-          events.map((event, i) => (
-            <Card key={i}>
-              <CardHeader>
-                <CardTitle className="flex justify-between items-center">
-                  {event.title}
-                </CardTitle>
-              </CardHeader>
-              <CardContent>
-                <div>Description: {event.description}</div>
-                <div>{`Event start: ${event.date_start_event} ${event.time_start_event}`}</div>
-                <div>{`Next event: ${
-                  event.interval === -1
-                    ? "TW"
-                    : event.interval === 0
-                    ? "Never"
-                    : `${event.interval} days`
-                }`}</div>
-                <div>{`Activity time: ${event.activity_time} hours`}</div>
-                <div>{`Channel ID: ${
-                  discordData?.channels.find((e) => e.id === event.channel_id)
-                    ?.label
-                }`}</div>
-                <div>{`Role ID: ${
-                  discordData?.roles.find((e) => e.id === event.role_id)?.label
-                }`}</div>
-                <div>{`List: ${event.signUps.length}`}</div>
-              </CardContent>
-              <CardFooter className="flex justify-end gap-4">
-                <span>Actions:</span>
-                {discordDataLoading || !discordData ? null : (
-                  <EventDialog
-                    disabled={limited}
-                    discordData={discordData}
-                    house={house}
-                    entry={event}
-                    roleId={config.member.id}
-                  />
-                )}
-                <TooltipProvider>
-                  <Tooltip>
-                    <TooltipTrigger asChild>
-                      <Button
-                        variant="destructive"
-                        className="rounded-full p-2 h-fit"
-                        onClick={() => onDelete(event)}
-                      >
-                        <Trash2 className="w-4 h-4" />
-                      </Button>
-                    </TooltipTrigger>
-                    <TooltipContent>
-                      <p>Delete</p>
-                    </TooltipContent>
-                  </Tooltip>
-                </TooltipProvider>
-              </CardFooter>
-            </Card>
-          ))
-        )}
+        {events.map((event, i) => (
+          <Card key={i}>
+            <CardHeader>
+              <CardTitle className="flex justify-between items-center">
+                {event.title}
+              </CardTitle>
+            </CardHeader>
+            <CardContent>
+              <div>Description: {event.description}</div>
+              <div>{`Event start: ${event.date_start_event} ${event.time_start_event}`}</div>
+              <div>{`Next event: ${
+                event.interval === -1
+                  ? "TW"
+                  : event.interval === 0
+                  ? "Never"
+                  : `${event.interval} days`
+              }`}</div>
+              <div>{`Activity time: ${event.activity_time} hours`}</div>
+              <div>{`Channel ID: ${
+                discordData?.channels.find((e) => e.id === event.channel_id)
+                  ?.label
+              }`}</div>
+              <div>{`Role ID: ${
+                discordData?.roles.find((e) => e.id === event.role_id)?.label
+              }`}</div>
+              <div>{`List: ${event.signUps.length}`}</div>
+            </CardContent>
+            <CardFooter className="flex justify-end gap-4">
+              <span>Actions:</span>
+              {discordDataLoading || !discordData ? null : (
+                <EventDialog
+                  disabled={limited}
+                  discordData={discordData}
+                  house={house}
+                  entry={event}
+                  roleId={config.member.id}
+                />
+              )}
+              <Button
+                variant="destructive"
+                className="rounded-full p-2 h-fit"
+                onClick={() => onDelete(event)}
+              >
+                <Trash2 className="w-4 h-4" />
+              </Button>
+            </CardFooter>
+          </Card>
+        ))}
       </div>
       {discordDataLoading || !discordData ? null : (
         <div className="absolute bottom-4 right-4 gap-2 flex">
