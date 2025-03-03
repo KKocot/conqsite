69643e38207ac104199d72cde266bed0d29b6d8f
Krzysztof Kocot <k.kocot0@gmail.com>
2025-03-03 21:37:05 +0100

    Add other Activities to user stats

--- src/models/userStats.ts
+++ src/models/userStats.ts
@@ -1,10 +1,17 @@
 import mongoose, { Schema } from "mongoose";
+import { date } from "zod";
+
+const otherActivitiesSchema: Schema = new Schema({
+  date: String,
+  type: String,
+});
 
 const userStatsSchema: Schema = new Schema({
   id: String,
   nick: String,
   house: [String],
   attendance: [String],
+  otherActivities: { type: [otherActivitiesSchema], required: false },
 });
 
 const UserStats =
