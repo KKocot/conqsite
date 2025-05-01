7d68c69b64ae819e7f472c7fc1d0a0d3006d4554
Krzysztof Kocot <k.kocot0@gmail.com>
2025-05-01 11:04:52 +0200

    Fix doctrine card

--- src/feature/doctrines/doctrines-list-item.tsx
+++ src/feature/doctrines/doctrines-list-item.tsx
@@ -15,17 +15,19 @@ import Image from "next/image";
 const DoctrinesLisItem = ({ doctrine }: { doctrine: DoctrineType }) => {
   const statsList = doctrine.stats.split(". ").filter(Boolean);
   return (
-    <Card className="w-[260px] h-full">
+    <Card className="w-[260px] flex flex-col">
       <CardHeader>
         <CardTitle className="text-lg h-14">{doctrine.name}</CardTitle>
       </CardHeader>
       <CardContent className="flex flex-col items-center">
-        <Avatar className="w-1/2 h-1/2 rounded-none">
-          <AvatarImage src={doctrine.img} alt={`${doctrine.name} avatar`} />
-          <AvatarFallback>
-            <Image src="/logo.png" alt="avatar" width={362} height={362} />
-          </AvatarFallback>
-        </Avatar>
+        <div className="w-64 h-32 flex justify-center items-center">
+          <Avatar className="w-1/2 h-full rounded-none">
+            <AvatarImage src={doctrine.img} alt={`${doctrine.name} avatar`} />
+            <AvatarFallback>
+              <Image src="/logo.png" alt="avatar" width={362} height={362} />
+            </AvatarFallback>
+          </Avatar>
+        </div>
         <ul className="mt-2">
           {statsList.map((stat, index) => (
             <li key={index} className="text-sm">
@@ -34,8 +36,8 @@ const DoctrinesLisItem = ({ doctrine }: { doctrine: DoctrineType }) => {
           ))}
         </ul>
       </CardContent>
+      <div className="flex-grow" />
       <Separator className="w-full my-4" />
-
       <CardFooter className="flex flex-col">
         <span className="text-sm font-semibold">For Unit:</span>
         {doctrine.forUnit.map((unit, i) => (
