b6057b3de6de5d80bbedb7e3c6885d1fa63d9cd4
Krzysztof Kocot <k.kocot0@gmail.com>
2025-06-29 06:56:26 +0200

    Improve map page

--- src/app/(pages)/dashboard/maps/[map]/content.tsx
+++ src/app/(pages)/dashboard/maps/[map]/content.tsx
@@ -14,6 +14,18 @@ const Content = ({ data }: { data: MapAsset }) => {
       <div className="flex flex-col gap-4 justify-center items-center p-12 justify-self-center">
         <h1 className="text-5xl font-bold">{data.name}</h1>
         <div>{data.type}</div>
+        {data.cities.length > 0 ? (
+          <div>
+            <span>Occurrence:</span>
+            <ul className="font-semibold">
+              {data.cities.map((e) => (
+                <li key={e} className="list-disc">
+                  {e}
+                </li>
+              ))}
+            </ul>
+          </div>
+        ) : null}
         {data.types.map((type) => (
           <ImageComponent
             key={`${data.name}-${type}`}
