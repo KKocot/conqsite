bc76bcda43d2a19f01b5228e0c75d4233d568f47
Krzysztof Kocot <k.kocot0@gmail.com>
2025-08-27 10:06:20 +0200

    fix stripe error

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,2 +1,12 @@
+// import Stripe from "stripe";
+// export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
 import Stripe from "stripe";
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
+
+export function getStripe() {
+  if (!process.env.STRIPE_SECRET_KEY) {
+    throw new Error("Missing STRIPE_SECRET_KEY");
+  }
+  return new Stripe(process.env.STRIPE_SECRET_KEY);
+}
+
+export const stripe = getStripe();
